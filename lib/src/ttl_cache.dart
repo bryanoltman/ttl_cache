4df3314f7ca2bd3908b82cdc9bdfe67027d8077d
Bryan Oltman <bryan@shorebird.dev>
2024-02-16 22:54:52 -0500

    implement TtlCache

--- lib/src/ttl_cache.dart
+++ lib/src/ttl_cache.dart
@@ -1,7 +1,69 @@
+import 'package:clock/clock.dart';
+
 /// {@template ttl_cache}
 /// A simple key-value store with entry expiration
 /// {@endtemplate}
-class TtlCache {
+class TtlCache<K, V> {
   /// {@macro ttl_cache}
-  const TtlCache();
+  TtlCache({this.defaultTtl});
+
+  /// The amount of time before entries are removed from the cache if no TTL is
+  /// provided.
+  final Duration? defaultTtl;
+
+  /// The backing key-value store.
+  final _cache = <K, V>{};
+
+  /// Maps keys to their expiration times.
+  final _expirations = <K, DateTime>{};
+
+  /// Adds a key-value pair to the cache that expires after [ttl]. If no [ttl]
+  /// is provided, the entry will never expire.
+  void set(K key, V value, {Duration? ttl}) {
+    _cache[key] = value;
+
+    final effectiveTtl = ttl ?? defaultTtl;
+    if (effectiveTtl != null) {
+      _expirations[key] = clock.now().add(effectiveTtl);
+
+      // Clean up expired entries after [ttl] has elapsed if needed.
+      Future.delayed(effectiveTtl, () => _removeIfExpired(key));
+    }
+  }
+
+  /// Retrieves the value associated with [key] if it exists and has not
+  /// expired.
+  V? get(K key) {
+    _removeIfExpired(key);
+    return _cache[key];
+  }
+
+  /// Removes the entry for [key] if it exists.
+  void remove(K key) {
+    _cache.remove(key);
+    _expirations.remove(key);
+  }
+
+  /// Returns the expiration time of the entry associated with [key] if the key
+  /// exists and has a TTL.
+  DateTime? getExpiration(K key) {
+    _removeIfExpired(key);
+    return _expirations[key];
+  }
+
+  /// Looks up the value associated with [key] if it exists and has not expired.
+  V? operator [](K key) => get(key);
+
+  /// Associates [key] with [value] in the cache, using [defaultTtl] as the TTL.
+  void operator []=(K key, V value) => set(key, value);
+
+  /// Removes the entry for [key] if it exists and has an expiration date in the
+  /// past.
+  void _removeIfExpired(K key) {
+    final expiration = _expirations[key];
+    if (expiration != null && clock.now().isAfter(expiration)) {
+      _cache.remove(key);
+      _expirations.remove(key);
+    }
+  }
 }
--- test/src/ttl_cache_test.dart
+++ test/src/ttl_cache_test.dart
@@ -1,11 +1,93 @@
 // ignore_for_file: prefer_const_constructors
+
+import 'package:clock/clock.dart';
 import 'package:test/test.dart';
 import 'package:ttl_cache/ttl_cache.dart';
 
 void main() {
   group('TtlCache', () {
-    test('can be instantiated', () {
-      expect(TtlCache(), isNotNull);
+    group('with no TTL set', () {
+      test('entries never expire', () {
+        var time = DateTime(2021);
+        withClock(Clock(() => time), () {
+          final cache = TtlCache<String, String>();
+          cache['foo'] = 'bar';
+          cache.set('hello', 'world');
+
+          expect(cache['foo'], equals('bar'));
+          expect(cache.get('hello'), equals('world'));
+          expect(cache.getExpiration('foo'), isNull);
+          expect(cache.getExpiration('hello'), isNull);
+
+          time = time.add(Duration(days: 99999));
+          // Advancing a long time into the future should not cause the entries
+          // to expire.
+          expect(cache['foo'], equals('bar'));
+          expect(cache.get('hello'), equals('world'));
+          expect(cache.getExpiration('foo'), isNull);
+          expect(cache.getExpiration('hello'), isNull);
+        });
+      });
+    });
+
+    group('with a TTL set', () {
+      test('entries expire after the TTL', () {
+        var time = DateTime(2021);
+        withClock(Clock(() => time), () {
+          final cache =
+              TtlCache<String, String>(defaultTtl: Duration(seconds: 1));
+          cache['foo'] = 'bar';
+          cache.set('hello', 'world', ttl: Duration(seconds: 2));
+
+          final fooExpiration = time.add(Duration(seconds: 1));
+          final helloExpiration = time.add(Duration(seconds: 2));
+
+          expect(cache['foo'], equals('bar'));
+          expect(cache.get('hello'), equals('world'));
+          expect(cache.getExpiration('foo'), equals(fooExpiration));
+          expect(cache.getExpiration('hello'), equals(helloExpiration));
+
+          time = time.add(Duration(seconds: 1));
+          // Both entries should still be in the cache. 'foo' is at but not past
+          // its expiration time.
+          expect(cache['foo'], equals('bar'));
+          expect(cache.get('hello'), equals('world'));
+          expect(cache.getExpiration('foo'), equals(fooExpiration));
+          expect(cache.getExpiration('hello'), equals(helloExpiration));
+
+          time = time.add(Duration(seconds: 1));
+          // 'foo' should be expired now, but 'hello' should still be in the
+          // cache.
+          expect(cache['foo'], isNull);
+          expect(cache.get('hello'), equals('world'));
+          expect(cache.getExpiration('foo'), isNull);
+          expect(cache.getExpiration('hello'), equals(helloExpiration));
+
+          time = time.add(Duration(seconds: 1));
+          // Both keys should be expired now.
+          expect(cache['foo'], isNull);
+          expect(cache.get('hello'), isNull);
+          expect(cache.getExpiration('foo'), isNull);
+          expect(cache.getExpiration('hello'), isNull);
+        });
+      });
+
+      test('can remove an entry with an expiration date in the future', () {
+        final time = DateTime(2021);
+        withClock(Clock.fixed(time), () {
+          final cache = TtlCache<int, String>()
+            ..set(1, 'one', ttl: Duration(seconds: 1));
+          final expirationDate = time.add(Duration(seconds: 1));
+
+          expect(cache.get(1), equals('one'));
+          expect(cache.getExpiration(1), equals(expirationDate));
+
+          cache.remove(1);
+
+          expect(cache.get(1), isNull);
+          expect(cache.getExpiration(1), isNull);
+        });
+      });
     });
   });
 }
